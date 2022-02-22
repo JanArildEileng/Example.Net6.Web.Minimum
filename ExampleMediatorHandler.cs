dc41eba2ae6aaa7aa954d50202ae1218632ca19b
Jan Eileng <janarild.eileng@fhi.no>
2022-02-22 11:47:23 +0100

    Added MediatR

---
+++ ExampleMediatorHandler.cs
@@ -0,0 +1,12 @@
+
+using MediatR;
+
+public class HelloMediatr : IRequest<string> { }
+
+public class HelloMediatrHandler : IRequestHandler<HelloMediatr, string>
+{
+    public Task<string> Handle(HelloMediatr request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("Hello fra HelloMediatr");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMediatR(typeof(HelloMediatrHandler));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello MediatR!");
+app.MapGet("/HelloMediatr", async(IMediator mediator ) => await mediator.Send(new HelloMediatr()) );
+
 
 app.Run();
