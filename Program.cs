e5b03d569e5b8c6dcf7e6709103685246c44d278
Jan Eileng <janarild.eileng@fhi.no>
2022-02-22 09:20:25 +0100

    Added Swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using Swashbuckle.AspNetCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });                
+    });
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger((option)=>{
+}).UseSwaggerUI();
+
+
+app.MapGet("/", () => "Hello World with Swagger!");
 
 app.Run();
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+
+#Swagger
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-6.0&tabs=visual-studio
