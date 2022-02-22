ccae265411d1ecb3df72849000a213f347b1ee6e
Jan Eileng <janarild.eileng@fhi.no>
2022-02-22 11:14:54 +0100

    Add MediatR

--- Program.cs
+++ Program.cs
@@ -1,22 +1,51 @@
+
+using Serilog;
 using Swashbuckle.AspNetCore;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+//Sett om logging med Serilog , uten å bruke Appsetting.json
+Log.Logger= new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+
+try {
+    Log.Information("Hello, world Serilog!");
+
+    var builder = WebApplication.CreateBuilder(args);
+ //       builder.Logging.AddSerilog();  //bruk Serilog i tilegg til annen logging
+
+    //bruk Appsetting.json til å konfigurer Serilog
+     var serilogger= new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+    builder.Host.UseSerilog(serilogger);  //kun bruk Serilog til logging
+
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
     {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });                
     });
 
 
+    var app=builder.Build();
+    app.UseSwagger((option)=>{
+    }).UseSwaggerUI();
 
-var app = builder.Build();
-
-app.UseSwagger((option)=>{
-}).UseSwaggerUI();
+    app.MapGet("/", () => "Hello World with Swagger!");
 
 
-app.MapGet("/", () => "Hello World with Swagger!");
+  
+    app.MapGet("/", () => "Hello World!");
+    app.Run();
 
-app.Run();
+} catch (Exception ex)  {
+     Log.Error(ex, "Something went wrong");
+}  finally  {
+    Log.Information("Goodbye,Serilog!");
+    Log.CloseAndFlush();
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@
 
 #Swagger
 https://docs.microsoft.com/en-us/aspnet/core/tutorials/getting-started-with-swashbuckle?view=aspnetcore-6.0&tabs=visual-studio
+#Serilog
+https://github.com/serilog/serilog-aspnetcore#serilogaspnetcore---
+https://andrewlock.net/exploring-dotnet-6-part-2-comparing-webapplicationbuilder-to-the-generic-host/
