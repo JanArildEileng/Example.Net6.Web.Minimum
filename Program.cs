cf6ee38089d2d923db6c40a02d9445af00c45493
Jan Eileng <janarild.eileng@fhi.no>
2022-02-22 11:14:54 +0100

    Add Serilog

--- Program.cs
+++ Program.cs
@@ -1,6 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Serilog;
 
-app.MapGet("/", () => "Hello World!");
+//Sett om logging med Serilog , uten å bruke Appsetting.json
+Log.Logger= new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
-app.Run();
+try {
+    Log.Information("Hello, world Serilog!");
+
+    var builder = WebApplication.CreateBuilder(args);
+ //       builder.Logging.AddSerilog();  //bruk Serilog i tilegg til annen logging
+
+    //bruk Appsetting.json til å konfigurer Serilog
+     var serilogger= new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+    builder.Host.UseSerilog(serilogger);  //kun bruk Serilog til logging
+
+    var app=builder.Build();
+
+
+  
+    app.MapGet("/", () => "Hello World!");
+    app.Run();
+
+} catch (Exception ex)  {
+     Log.Error(ex, "Something went wrong");
+}  finally  {
+    Log.Information("Goodbye,Serilog!");
+    Log.CloseAndFlush();
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Serilog
+https://github.com/serilog/serilog-aspnetcore#serilogaspnetcore---
+https://andrewlock.net/exploring-dotnet-6-part-2-comparing-webapplicationbuilder-to-the-generic-host/
